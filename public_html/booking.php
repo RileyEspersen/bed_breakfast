481387ac6b56562c2534c96f3711d129129a23a4
Riley Espersen <riley.espersen@my.simpson.edu>
2018-10-31 15:50:45 -0500

    php changes

--- public_html/booking.php
+++ public_html/booking.php
@@ -12,36 +12,9 @@
 
 	<body>
 		<img src="img/logo.png" alt="Simpson Bed and Breakfast logo" class = "center">
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Simpson Bed and Breakfast</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor02">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="index.html">Home</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="about.html">About <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="housing.html">Our Rooms</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="breakfast.html">Breakfast</a>
-			</li>
-			<li class="nav-item active">
-				<a class="nav-link" href="booking.html">Booking</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="thingstodo.html">Things To Do</a>
-			</li>
-		</ul>
-			</div>
-			
-		</nav>
+		
+		<?php include '../resources/nav.php'; ?>
+		
 		<header>
 			<h1>Book A Room</h1>
 		</header>
@@ -89,11 +62,9 @@
 		</div>
 		<br>
 		<br>
-		<footer>
-		<div class="container">
-			 <p>&copy; 2018 Simpson Bed and Breakfast</p>
-		</div>
-		</footer>
+		
+		<?php include '../resources/footenote.php'; ?>
+		
 			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
---
+++ resources/footnote.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="container">
+		<p>&copy; 2018 Simpson Bed and Breakfast</p>
+	</div>
+</footer>
